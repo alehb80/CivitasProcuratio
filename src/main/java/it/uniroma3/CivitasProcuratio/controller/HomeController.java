770c083e2709a5e16f477210987bdac88eb19ea2
Alessio Ligios <alessyo_93@me.com>
2018-07-19 17:22:40 +0200

    Minor fix.

--- src/main/java/it/uniroma3/CivitasProcuratio/controller/HomeController.java
+++ src/main/java/it/uniroma3/CivitasProcuratio/controller/HomeController.java
@@ -3,9 +3,6 @@ package it.uniroma3.CivitasProcuratio.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.LocalDateTime;
-import java.util.Calendar;
-
 @Controller
 public class HomeController {
 
